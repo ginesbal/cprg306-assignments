db69261714681da87fd49a349a9d61fd84babadd
ginesbal <ginesehrl.balquin@edu.sait.ca>
2024-05-27 19:59:40 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <p>Ehrl Balquin</p>
+        <Link href= "https://github.com/ginesbal">Github Link</Link>
+      </div>
+    );
+  }
+
